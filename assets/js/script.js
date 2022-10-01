483174b5c43235db9dcd516fe150f66544d1fdee
João Suzuki <joaosuzuki98@outlook.com>
2022-10-01 19:37:45 -0300

    Js function to randomize color classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
-const getDate = () => {
-    
+// Functions
+const randomColorGenerator = () => {
+    const contentList = document.getElementsByClassName('content')
+
+    Array.from(contentList).forEach(handleColor)
 }
+
+
+
+const handleColor = (value) => {
+    const colors = ['green', 'red', 'blue', 'purple', 'orange', 'dark-green', 'dark-red', 'dark-blue', 'dark-purple']
+
+    var randomColor = colors[Math.floor(Math.random() * colors.length)]
+
+    value.classList.add(randomColor)
+}
+
+// Function calls
+randomColorGenerator()
